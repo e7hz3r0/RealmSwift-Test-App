15b312033fb501c813d955fb488cd9e288ba6bd7
Ethan Urie <ethan.urie@gmail.com>
2015-05-26 20:21:23 -0400

    Initial Commit

---
+++ RealmSwift Test App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSwift Test App
+//
+//  Created by Ethan Urie on 5/26/15.
+//  Copyright (c) 2015 Strange Quark Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RealmSwift Test AppTests/RealmSwift_Test_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  RealmSwift_Test_AppTests.swift
+//  RealmSwift Test AppTests
+//
+//  Created by Ethan Urie on 5/26/15.
+//  Copyright (c) 2015 Strange Quark Software. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RealmSwift_Test_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
