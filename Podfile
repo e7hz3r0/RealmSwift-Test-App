30ef4a51fcfa6deb6fa52455bcf001975949a491
Ethan Urie <ethan.urie@gmail.com>
2015-05-29 09:57:39 -0400

    Basically not using CocoaPods to get RealmSwift to work.

--- Podfile
+++ Podfile
@@ -3,11 +3,8 @@
 use_frameworks!
 
 target 'RealmSwift Test App' do
-    pod 'evernote-cloud-sdk-ios', '~> 2.0'
-    pod 'RealmSwift'
 end
 
 target 'RealmSwift Test AppTests' do
-    pod 'RealmSwift'
 end
 
--- RealmSwift Test App/AppDelegate.swift
+++ RealmSwift Test App/AppDelegate.swift
@@ -13,7 +13,7 @@ import RealmSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var session:ENSession?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
